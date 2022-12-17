4f08f405ac9d8e088d33fe3f720947d205f3c4e9
MohonSaha <practicewithmohon@gmail.com>
2022-12-17 23:47:20 +0600

    adding more problem

---
+++ Problem29.js
@@ -0,0 +1,14 @@
+// Array value index concept 
+
+var string = "Hello";
+var strLen = string.length;
+console.log(strLen);
+var carecter1 = string[0];
+console.log(carecter1);
+var carecter2 = string[1];
+console.log(carecter2);
+
+var char5 = string[4];
+console.log(char5);
+var carecter5 = string[strLen-1];
+console.log(carecter5);
---
+++ Problem30.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array (a) as argument
+// Extract the first 3 elements of a
+
+function myFunction(a) {
+    let sum = [];
+        for(i = 0; i<= 2; i++){
+            var three = a[i];
+            let = sum.push(three);
+        }
+    console.log(sum);
+}
+myFunction([1, 2, 3, 4])
